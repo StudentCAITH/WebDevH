567ede4094d7d951ca4172e393066f87cfd7ecb6
StudentCAITH <142339005+StudentCAITH@users.noreply.github.com>
2024-03-11 11:00:04 -0400

    Button
    Added a button to JavaIntro.

--- JavaIntro/main.js
+++ JavaIntro/main.js
@@ -1,6 +1,12 @@
 const myHeading = document.querySelector("span");
 const myAlt = document.querySelector("h2");
 myHeading.textContent = "Why is it so stupid?";
-let Var = "Loud";
-if (Var == "Quiet")
-{myAlt.textContent = "ERROR";}
+let Var = "Quiet";
+let myButton = document.querySelector("button");
+function setError() {
+    myAlt.textContent = "ERROR ERROR";
+    myHeading.textContent = "WHY?";
+}
+myButton.onclick = () => {
+    setError();
+}
