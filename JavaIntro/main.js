b0b76eaa12a34ce2765941d28c89331f0772e36b
StudentCAITH <142339005+StudentCAITH@users.noreply.github.com>
2024-03-26 11:00:27 -0400

    Brachyura
    Completed error.
    Added collapsible images.
    Added the last boss.

--- JavaIntro/main.js
+++ JavaIntro/main.js
@@ -1,11 +1,24 @@
 const myHeading = document.querySelector("span");
-const myList = document.querySelector(".break");
+const myList = document.querySelectorAll(".break");
+const imageList = document.querySelector("img");
 myHeading.textContent = "Why is it so stupid?";
 let Var = "Quiet";
 let myButton = document.querySelector("button");
+let i = 0;
 function setError() {
-    myList.textContent = "ERROR ERROR";
     myHeading.textContent = "WHY?";
+    while(i < myList.length) {
+        myList[i].textContent = "ERROR ERROR";
+        i++;
+    }
+}
+imageList.onclick = () => {
+    const mySrc = imageList.getAttribute("src");
+    if (mySrc === "Small.png") {
+        imageList.setAttribute("src", "Peeping Big.png");
+    } else {
+        imageList.setAttribute("src", "Small.png");
+    }
 }
 myButton.onclick = () => {
     setError();
