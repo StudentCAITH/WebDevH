3bd7ad100a0b493d17c55c22b1154b7b69ab0f47
StudentCAITH <142339005+StudentCAITH@users.noreply.github.com>
2024-03-27 09:20:26 -0400

    Minor work
    ah

--- JavaIntro/main.js
+++ JavaIntro/main.js
@@ -1,23 +1,37 @@
-const myHeading = document.querySelector("span");
+const mySpan = document.querySelector(".one");
+const yourSpan = document.querySelector(".two")
 const myList = document.querySelectorAll(".break");
-const imageList = document.querySelector("img");
-myHeading.textContent = "Why is it so stupid?";
+const imageList = document.querySelectorAll("img");
+mySpan.textContent = "Why is it so stupid?";
 let Var = "Quiet";
 let myButton = document.querySelector("button");
 let i = 0;
 function setError() {
-    myHeading.textContent = "WHY?";
+    mySpan.textContent = "WHY?";
+    yourSpan.textContent = "AAAAAA";
     while(i < myList.length) {
         myList[i].textContent = "ERROR ERROR";
         i++;
     }
 }
-imageList.onclick = () => {
-    const mySrc = imageList.getAttribute("src");
+imageList[0].onclick = () => {
+    const mySrc = imageList[0].getAttribute("src");
     if (mySrc === "Small.png") {
-        imageList.setAttribute("src", "Peeping Big.png");
+        imageList[0].setAttribute("src", "Peeping Big.png");
     } else {
-        imageList.setAttribute("src", "Small.png");
+        imageList[0].setAttribute("src", "Small.png");
+    }
+}
+imageList[4].onclick = () => {
+    const mySrc = imageList[4].getAttribute("src");
+    if (mySrc === "Small.png") {
+        imageList[4].setAttribute("src", "Brachyura.png");
+        imageList[4].setAttribute("height", "400");
+        imageList[4].setAttribute("width", "500");
+    } else {
+        imageList[4].setAttribute("src", "Small.png");
+        imageList[4].setAttribute("height", "80");
+        imageList[4].setAttribute("width", "160");
     }
 }
 myButton.onclick = () => {
