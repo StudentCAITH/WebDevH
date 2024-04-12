e6cd7f3696db504b0bd452e2b6021630fe57c4a6
StudentCAITH <142339005+StudentCAITH@users.noreply.github.com>
2024-04-12 10:07:03 -0400

    JS Practice 1 Complete

--- JS_Practice/Practice_1/script.js
+++ JS_Practice/Practice_1/script.js
@@ -6,3 +6,10 @@ let myVar = 5;
 console.log(myVar);
 myVar += 5;
 console.log(myVar);
+const add_this = 2;
+
+function incrementNumber(initial, incrementor) {
+    return initial + incrementor;
+}
+
+console.log(incrementNumber(myVar, add_this));
