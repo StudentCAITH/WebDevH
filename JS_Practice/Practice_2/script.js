bb7eb34c0e3d0142c1319dec9562af52940c9394
StudentCAITH <142339005+StudentCAITH@users.noreply.github.com>
2024-04-14 19:35:17 -0400

    JS Domain 2
    Domain 2 Practice 2

--- JS_Practice/Practice_2/script.js
+++ JS_Practice/Practice_2/script.js
@@ -2,10 +2,10 @@
 Use this file to write your JavaScript code.
 Delete this comment when you start writing your code.
  */
-var x = true;
+var x = "true";
 var y = false;
 
-if (x == true) {
+if (x === true) {
     console.warn("You shouldn't be seeing this. Check your if statement");
 } else if (x == "true") {
     console.info("This statement should execute instead of the other one.");
@@ -14,3 +14,13 @@ if (x == true) {
 if (y == 0) {
     console.info("False and zero are the same in == conditionals.");
 }
+
+let a = Math.ceil(Math.sin(2));
+let z = Math.abs(-18);
+
+console.log(a);
+console.log(z);
+
+let final_value = Math.round(Math.sqrt(a + z));
+
+console.log("The final value is: " + final_value);
